e6d0dbcee96a5eeb2f1c210bdf3e25947513ccc9
Melkam22 <asheaddis@yahoo.com>
2022-08-05 15:29:54 +0200

    first route & basic styling added

---
+++ resources/views/cars.blade.php
@@ -0,0 +1,12 @@
+ 
+
+<div>
+    <h1 style="display: flex; justify-content: center; margin-top: 2%;">welcome to Cars Page</h1>
+</div>
+
+<br/>
+
+<nav style="height: 50px; width: 100vw; background: #0c63e4; display: flex">
+    <a href="books" style="margin-top: 7px; margin-left: 10px;"> <button style="height: 34px;">All Cars List</button></a>
+    <a href="home" style="margin-top: 7px; margin-left: 10px;"> <button style="height: 34px;">Add New Car</button></a>
+</nav>
